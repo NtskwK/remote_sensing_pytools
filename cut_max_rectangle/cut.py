7939cce13d56006c481f3de9e802d5ca65115a8a
Ntkskwk <natsukawa247@outlook.com>
2023-08-07 23:08:57 +0800

    feat: cut normal roi

--- cut_max_rectangle/cut.py
+++ cut_max_rectangle/cut.py
@@ -1,36 +1,73 @@
 import time
+import logging
 import rasterio
-from rasterio.windows import Window
-from rasterio.transform import Affine
+import numpy as np
 import PySimpleGUI as sg
 
-def cut(file_path, cropped_file_path):
-    with rasterio.open(file_path) as src:
-        band_index = 0
-        band_data = src.read(band_index+1)
-        
-        top, bottom, left, right = find_largest_rectangle(band_data)
-        
-        window = Window(left, top, right - left + 1, bottom - top + 1)
-        
-        cropped_data = src.read(window=window)
-        
-        metadata = src.meta
-        metadata.update({
-            "height": window.height,
-            "width": window.width,
-            "transform": window_transform(src.transform, window),
-        })
+from rasterio.transform import Affine
+from rasterio.windows import Window
 
-    with rasterio.open(cropped_file_path, "w", **metadata) as dest:
-        dest.write(cropped_data)
+def crop_and_save_tiff(tiff_path, output_path):
+    try:
+        with rasterio.open(tiff_path) as dataset:
+            num_bands = dataset.count
+            metadata = dataset.meta
+
+            all_bands_data = []
+            for i in range(1, num_bands+1):
+                band_data = dataset.read(i)
+                all_bands_data.append(band_data)
+
+            stacked_data = np.stack(all_bands_data, axis=0)
+            mask = np.all(stacked_data > 0, axis=0)
+            cropped_data = stacked_data[:, mask]
+
+            metadata.update(count=cropped_data.shape[0], height=cropped_data.shape[1], width=cropped_data.shape[2])
+
+            with rasterio.open(output_path, 'w', **metadata) as output_dataset:
+                for i, band_data in enumerate(cropped_data, start=1):
+                    output_dataset.write(band_data, i)
+
+        logging.info('mission compelet!')
+
+    except Exception as e:
+        logging.error(f'error: {str(e)}')
+
+
+def cut_rectangle(file_path, cropped_file_path):
+    try:
+        with rasterio.open(file_path) as src:
+            band_index = 0
+            band_data = src.read(band_index+1)
+
+            top, bottom, left, right = find_largest_rectangle(band_data)
+
+            window = Window(left, top, right - left + 1, bottom - top + 1)
+
+            cropped_data = src.read(window=window)
+
+            metadata = src.meta
+            metadata.update({
+                "height": window.height,
+                "width": window.width,
+                "transform": window_transform(src.transform, window),
+            })
+
+        with rasterio.open(cropped_file_path, "w", **metadata) as dest:
+            dest.write(cropped_data)
+
+        logging.info('mission compelet!')
+
+    except Exception as e:
+        logging.error(f'error: {str(e)}')
+
 
 def find_largest_rectangle(image_data):
     mask = (image_data > 0)
-    
+
     left_edges = []
     right_edges = []
-    
+
     for row in mask:
         left_edge = next((i for i, value in enumerate(row) if value), None)
         right_edge = next((i for i, value in enumerate(row[::-1]) if value), None)
@@ -40,21 +77,21 @@ def find_largest_rectangle(image_data):
         else:
             left_edges.append(left_edge)
             right_edges.append(image_data.shape[1] - right_edge - 1)
-    
+
     max_area = 0
     best_top, best_bottom, best_left, best_right = 0, 0, 0, 0
-    
+
     for top in range(image_data.shape[0]):
         for bottom in range(top, image_data.shape[0]):
             width = min(right_edges[top:bottom + 1]) - max(left_edges[top:bottom + 1]) + 1
             area = width * (bottom - top + 1)
-            
+
             if area > max_area:
-                print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time)) + ':找到新的最大矩形' + f'{max_area}', end='\r')
+                logging.info(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time())) + ':the largest rectangle' + f'{max_area}')
                 max_area = area
                 best_top, best_bottom = top, bottom
                 best_left, best_right = max(left_edges[top:bottom + 1]), min(right_edges[top:bottom + 1])
-    
+
     return best_top, best_bottom, best_left, best_right
 
 
@@ -63,47 +100,43 @@ def window_transform(transform, window):
     return transform_window
 
 
-def GUI():
-    sg.theme('DarkBlue1')
+logging.basicConfig(filename='progress.log', level=logging.INFO, format='%(asctime)s %(message)s')
 
-    layout = [    
-                [sg.Text('裁剪南北走向面积最大的矩形', font=("Source Han Sans", 28),
-                    pad=((10, 50), (50, 50)),justification='center')],
-                [sg.Text('选择要裁剪的文件(只支持tif)', font=("Source Han Sans", 16),
-                    justification='left')],
-                [sg.Input(key='-TIF-'), 
-                     sg.FileBrowse(file_types=(('TIF Files', '*.tif'),))],
-                [sg.Text('选择输出目录：', font=("Source Han Sans", 16),
-                    justification='left')],
-                [sg.Input(key='-DIR-'), 
-                     sg.FolderBrowse()],
-                [sg.Column(
-                    [[sg.Button('确定', key='confirm', font=("Source Han Sans", 16), pad=(10, 0)), 
-                        sg.Button('取消', key='cancel', font=("Source Han Sans", 16), pad=(10, 0))]],
-                    )]
-        ]
+layout = [
+    [sg.Text('Select the GeoTIFF file:')],
+    [sg.Input(key='-INPUT-', enable_events=True), sg.FileBrowse(file_types=(("TIFF Files", "*.tif"),))],
+    [sg.Text('Output:')],
+    [sg.Input(key='-OUTPUT-'), sg.FileSaveAs(file_types=(("TIFF Files", "*.tif"),))],
+    [sg.Checkbox('Cut to a north-south rectangle', key='-CROP-', default=False)],
+    [sg.Button('Apply')]
+]
 
-    window = sg.Window('cut rectangle', layout, default_element_size=(30, 1), size=(None, None))
+window = sg.Window('GeoTIFF Clipper', layout)
 
-    while True:
-        event, values = window.read(close=False)
-        if event == sg.WIN_CLOSED or event == 'cancel':
-            quit()
+while True:
+    event, values = window.read()
 
-        input, output = [values[x] for x in values]
+    if event == sg.WINDOW_CLOSED:
+        break
 
-        start_time = time.time()
-        print(f'{time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(start_time))}' + ':任务开始')
-        cut(input, output)
+    if event == 'Apply':
+        input_file = values['-INPUT-']
+        output_file = values['-OUTPUT-']
+        crop_rectangular = values['-CROP-']
 
-        end_time = time.time()
+        if not (input_file and output_file):
+            sg.popup('Please select the input and output file paths!')
 
-        duration = end_time - start_time
+        try:
+            if crop_rectangular:
+                cut_rectangle(input_file, output_file)
+            else:
+                crop_and_save_tiff(input_file, output_file)
+            sg.popup('mission compelet!')
 
-        print('\n' + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(start_time)) + ':\n代码块执行时间:', int(duration), '秒')
+        except Exception as e:
+            sg.popup(f'error: {str(e)}')
 
-    window.close()
+        logging.info('mission compelet!')
 
-
-if __name__ == '__main__':
-    GUI()
+window.close()
